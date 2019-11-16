1406e9f545338029e762eb3fc213e80a606a44aa
祐任 林 <andy_840123@yahoo.com.tw>
2019-11-15 23:04:51 -0800

    add map function

---
+++ Map.js
@@ -0,0 +1,90 @@
+$(document).ready(function() {
+  initMap(null);
+
+  load_items_on_maps().then(function(data) {
+    if(data) {
+      var len = data.length;
+      for (var i = 0; i < len; ++i) {
+        var Name = data[i].Name;
+        var Tag = data[i].Tag;
+        var Price = data[i].Price;
+        var Description = data[i].Description;
+        var GPS = data[i].GPS;
+        var Latitude = data[i].Latitude;
+        var Longitude = data[i].Longitude;
+        var PostTime = data[i].PostTime;
+        var Address = data[i].Address;
+      }
+
+      initMap(data);
+    }
+  });
+});
+
+
+function load_items_on_maps() {
+  return new Promise (function(resolve) {
+    $.ajax({
+      type: "POST",
+      url: "Map.php",
+      dataType: "JSON",
+      data: {
+        flag: 3
+      },
+      complete: function (data) {
+        resolve(data);
+      }
+    });
+  });
+}
+
+// For user's current location
+function initMap(data) {
+
+  var m = document.getElementById('map');
+
+  //add map options
+  var options = {
+    zoom:11,
+    center:{lat:44.565214,lng:-123.275675}
+  };
+
+  //add map
+  var map = new google.maps.Map(m,options);
+
+
+  // add marker on current location
+  var marker = new google.maps.Marker({
+    position:{lat:44.565214,lng:-123.275675},
+    map:map
+  });
+
+
+  var circle = new google.maps.Circle({
+    map: map,
+    radius: 8047,    // 5 miles in meters
+    fillColor: '#80807d'
+  });
+  circle.bindTo('center', marker, 'position');
+
+  if(data) {
+    var len = data.length;
+    for (var i = 0; i < len; ++i) {
+      var lat = data[i].Latitude;
+      var lon = data[i].Longitute;
+      if (lat && lon) {
+        setmarkers(lat, lon, map);
+      }
+    }
+  }
+}
+
+function setmarkers(lat, lon, map) {
+
+  var marker = new google.maps.Marker({
+    position:{lat:lat,lng:lon},
+    map:map
+  });
+
+}
+
---
+++ Map.php
@@ -0,0 +1,39 @@
+<?php
+
+function load_items($mysqli) {
+  $sql = "SELECT ItemID, Name, Tag, Price, Description, GPS, Latitude, Longitude, PostTime, Address FROM Item";
+
+  $stmt = $mysqli->prepare($sql);
+
+  $result_exe = $stmt->execute();
+
+  if ($result_exe) {
+    $result_bind = $stmt->bind_result($ItemID, $Name, $Tag, $Price, $Description, $GPS, $Latitude, $Longitude, $PostTime, $Address);
+    if ($result_bind) {
+      while ($stmt->fetch()) {
+        $return_arr[] = array(
+          "Name" => $Name,
+          "Tag" => $Tag,
+          "Price" => $Price,
+          "Description" => $Description,
+          "GPS" => $GPS,
+          "Latitude" => $Latitude,
+          "Longitude" => $Longitude,
+          "PostTime" => $PostTime,
+          "Address" => $Address
+
+        );
+      }
+      echo json_encode($return_arr);
+    }
+  }
+}
+
+if (isset($_POST["flag"]) && $_POST["flag"] == 3) {
+  include("db_conn.php");
+  load_items($mysqli);
+  $mysqli->close();
+  exit;
+}
+
+?>
