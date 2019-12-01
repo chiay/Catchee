62b425aae915c4b9fb3190e47077ab60d27ed5e5
Unknown <chiay@oregonstate.edu>
2019-11-30 22:52:11 -0800

    Update check message unit test

--- unit_test/test_check_message_table.php
+++ unit_test/test_check_message_table.php
@@ -8,12 +8,12 @@ $stmt = $mysqli->prepare($sql);
 $result_exe = $stmt->execute();
 
 if ($result_exe) {
-  $result_bind = $stmt->bind_result($mid, $usr1, $usr2, $content, $time);
+  $result_bind = $stmt->bind_result($mid, $cid, $uid, $content, $time);
   if ($result_bind) {
     while ($stmt->fetch()) {
       echo " MID: " . $mid .
-           " User1: " . $usr1 .
-           " User2: " . $usr2 .
+           " ChatID: " . $cid .
+           " UserID: " . $uid .
            " Content: " . $content .
            " Time: " . $time .
            " <br>";
